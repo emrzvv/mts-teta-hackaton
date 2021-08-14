1f58216e7056a554a56b7e7ed159a7da9dbbbf94
Blue water <blue.sugar.water@mail.ru>
2021-08-14 10:24:18 +0300

    Стартовый комит

---
+++ src/main/java/ru/mts/teta/hackaton/findmyphone/FindmyphoneApplication.java
@@ -0,0 +1,13 @@
+package ru.mts.teta.hackaton.findmyphone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindmyphoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindmyphoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mts/teta/hackaton/findmyphone/FindmyphoneApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mts.teta.hackaton.findmyphone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmyphoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
